5c63d2fe14a897f5bc2c2498172cd11433a47f44
md-mahin-islam-mahi <wdmahin.mhm@gmail.com>
2024-05-24 09:46:48 +0600

    Updated: Updated some script to check if everything is ok

--- install-scripts/sddm.sh
+++ install-scripts/sddm.sh
@@ -26,12 +26,52 @@ note="${megenta}[ NOTE ]${end}"
 done="${cyan}[ DONE ]${end}"
 error="${red}[ ERROR ]${end}"
 
+# install script dir
+ScrDir=`dirname "$(realpath "$0")"`
+source $ScrDir/1-global.sh
+
 # Set the name of the log file
 log="Install-Logs/sddm.log"
 
-# Set the script to exit on error
-set -e
+sddm_pkgs=(
+  libqt5-qtgraphicaleffects
+  libqt5-qtquickcontrols
+  libqt5-qtquickcontrols2
+  sddm-qt6
+  xauth
+  xf86-input-evdev
+  xorg-x11-server
+)
 
+# Install SDDM 
+printf "${action} - Installing sddm and dependencies.... \n"
+for sddm in "${sddm_pkgs[@]}" ; do
+  install_package_no_recommands "$sddm" "$log"
+done
+
+# Check if other login managers are installed and disabling their service before enabling sddm
+for login_manager in lightdm gdm lxdm lxdm-gtk3; do
+  if sudo  zypper se -i "$login_manager" &>> /dev/null; then
+    printf "${attention} - $login_manager and ${green}SDDM${end} will be enabled...\n"
+    sudo systemctl disable "$login_manager" 2>&1 | tee -a "$log"
+  fi
+done
+
+# activation of sddm service
+printf "${action} - Activating sddm service........\n"
+sudo systemctl set-default graphical.target 2>&1 | tee -a "$log"
+sudo update-alternatives --set default-displaymanager /usr/lib/X11/displaymanagers/sddm 2>&1 | tee -a "$log"
+sudo systemctl enable sddm.service 2>&1 | tee -a "$log"
+
+# setup wayland session
+wayland_sessions_dir=/usr/share/wayland-sessions
+if [ ! -d "$wayland_sessions_dir" ]; then
+printf "${action} - $wayland_sessions_dir not found, creating...\n"
+sudo mkdir -p "$wayland_sessions_dir" 2>&1 | tee -a "$log"
+sudo cp extras/hyprland.desktop "$wayland_sessions_dir/" 2>&1 | tee -a "$log"
+fi
+
+# setting sddm theme
 SDDM_THEME='extras/sddm-theme'
     printf "${action} - Setting up the login screen.\n"
     sudo cp -r $SDDM_THEME /usr/share/sddm/themes/
